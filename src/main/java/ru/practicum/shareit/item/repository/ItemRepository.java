3a5cfd60c2db8cd8a817508993749f5d0e4f8aa6
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-07-14 11:00:42 +0300

    Исправлены замечания по проекту.

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -16,5 +16,3 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
                                                                        Boolean available);
 
 }
-
-
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,14 @@
 DROP TABLE IF EXISTS requests, comments, bookings, items, users;
 
-CREATE TABLE IF NOT EXISTS users(
+
+CREATE TABLE IF NOT EXISTS users (
 id              BIGINT  GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 name            VARCHAR(300),
 email           VARCHAR(300),
 UNIQUE(email) );
+CREATE INDEX idx_users_id ON users (id);
+CREATE INDEX idx_users_name ON users (name);
+
 
 
 
@@ -14,6 +18,8 @@ description     VARCHAR(500),
 requester_id    BIGINT,
 created         TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
 CONSTRAINT fk_requests_to_users FOREIGN KEY(requester_id) REFERENCES users(id));
+CREATE INDEX idx_req_id ON requests (id);
+CREATE INDEX idx_req_description ON requests (description);
 
 
 
@@ -27,7 +33,8 @@ owner_id        BIGINT,
 request_id      BIGINT,
 CONSTRAINT fk_items_to_users FOREIGN KEY(owner_id) REFERENCES users(id),
 CONSTRAINT fk_items_to_request FOREIGN KEY(request_id) REFERENCES requests(id) );
-
+CREATE INDEX idx_items_id ON items (id);
+CREATE INDEX idx_items_name ON items (name);
 
 
 CREATE TABLE IF NOT EXISTS bookings (
@@ -39,6 +46,8 @@ booker_id       BIGINT,
 status          VARCHAR(300),
 CONSTRAINT fk_booking_to_items FOREIGN KEY(item_id) REFERENCES items(id),
 CONSTRAINT fk_booking_to_users FOREIGN KEY(booker_id) REFERENCES users(id) );
+CREATE INDEX idx_bookings_id ON bookings (id);
+CREATE INDEX idx_bookings_status ON bookings (status);
 
 
 CREATE TABLE IF NOT EXISTS comments (
@@ -49,6 +58,7 @@ author_id       BIGINT,
 created         TIMESTAMP WITHOUT TIME ZONE,
 CONSTRAINT fk_comments_to_items FOREIGN KEY(item_id) REFERENCES items(id),
 CONSTRAINT fk_comments_to_users FOREIGN KEY(author_id) REFERENCES users(id) );
+CREATE INDEX idx_comments_id ON comments (id);
 
 
 
